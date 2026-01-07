4d582b9632077b5fc1654636d33ac75374e895fa
darkhorse286 <darkhorse286@live.com>
2026-01-07 17:59:33 -0600

    removing escape for &

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "O'Brien &amp; Son",
+  title: "O'Brien & Son",
   description: "Collection of projects by Liam O'Brien",
 };
 
